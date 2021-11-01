7488ca22e87d60fa5934e197fb1daffbecf2c59a
Ramya G <ramya.gubbala@gmail.com>
2021-11-01 19:54:36 +0530

    add code in homecontroller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,6 +16,7 @@ namespace RKMaster.Controllers
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            //hello//
         }
 
         public IActionResult Index()
